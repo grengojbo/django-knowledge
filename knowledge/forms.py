317b4bf71f5a05256a5ce8bdec1618bf9916261f
Oleg Dolya <oleg.dolya@gmail.com>
2013-03-25 11:35:28 +0200

    modify knowlege

--- knowledge/forms.py
+++ knowledge/forms.py
@@ -73,6 +73,62 @@ def QuestionForm(user, *args, **kwargs):
     return _QuestionForm(*args, **kwargs)
 
 
+def QuestionAskForm(user, *args, **kwargs):
+    """
+    Build and return the appropriate form depending
+    on the status of the passed in user.
+    """
+
+    if user.is_anonymous():
+        if not settings.ALLOW_ANONYMOUS:
+            return None
+        else:
+            selected_fields = ['name', 'email', 'title', 'body']
+    else:
+        selected_fields = ['user', 'title', 'body', 'status']
+
+    if settings.ALERTS:
+        selected_fields += ['alert']
+
+    class _QuestionAskForm(forms.ModelForm):
+        def __init__(self, *args, **kwargs):
+            super(_QuestionAskForm, self).__init__(*args, **kwargs)
+
+            for key in self.fields:
+                if not key in OPTIONAL_FIELDS:
+                    self.fields[key].required = True
+
+            # hide the internal status for non-staff
+            qf = self.fields.get('status', None)
+            if qf and not user.is_staff:
+                choices = list(qf.choices)
+                choices.remove(('internal', _('Internal')))
+                qf.choices = choices
+
+            # a bit of a hack...
+            # hide a field, and use clean to force
+            # a specific value of ours
+            for key in ['user']:
+                qf = self.fields.get(key, None)
+                if qf:
+                    qf.widget = qf.hidden_widget()
+                    qf.required = False
+
+        # honey pot!
+        phone_number = forms.CharField(label=_('Phone number'), required=False)
+        captcha = ReCaptchaField(attrs={'theme': 'clean', 'lang': 'ru'})
+        categories = forms.HiddenInput(value=2)
+
+        def clean_user(self):
+            return user
+
+        class Meta:
+            model = Question
+            fields = selected_fields
+
+    return _QuestionAskForm(*args, **kwargs)
+
+
 def ResponseForm(user, question, *args, **kwargs):
     """
     Build and return the appropriate form depending
